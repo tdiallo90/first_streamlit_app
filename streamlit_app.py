87eaa5d64b80eff41c6aa05fa54bbd8d8530fc99
tdiallo90 <51756599+tdiallo90@users.noreply.github.com>
2023-10-03 20:59:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner!')
 
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Oeufs et Pomme de terre')
+streamlit.text('Croissant et lait de vache')
+streamlit.text('Boeuf sauté au porc')
+
