bdb70903ada8da526d06c8381fbd3d0c147d9813
tdiallo90 <51756599+tdiallo90@users.noreply.github.com>
2023-10-03 21:01:45 +0200

    Update streamlit_app.py
    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 streamlit.title('My Parent New Healthy Diner!')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Oeufs et Pomme de terre')
-streamlit.text('Croissant et lait de vache')
-streamlit.text('Boeuf sauté au porc')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
