46e98eb6468aad716fc71d2eb58072dc5a9fe76e
tdiallo90 <51756599+tdiallo90@users.noreply.github.com>
2023-10-04 19:07:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.header('Fruitvice Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# JSON Data Normalization 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Showing Normalized Data - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
 
